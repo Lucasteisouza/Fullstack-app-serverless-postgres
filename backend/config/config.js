3847e8ce3764c738883ab50489a44054ad39fb05
Lucasteisouza <lucasteisouza@gmail.com>
2024-03-03 16:02:45 -0300

    configurações de variaveis de ambiente

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const config = {
-  url: process.env.DB_URL,
-  dialect: 'postres',
+  url: process.env.POSTGRES_URL,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  dialect: 'postgres',
 };
 
 module.exports = {
