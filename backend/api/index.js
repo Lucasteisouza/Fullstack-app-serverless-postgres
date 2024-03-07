a2d804a344bf70bcfaff242e27eeea6e18186fa7
Lucasteisouza <lucasteisouza@gmail.com>
2024-03-07 16:38:26 -0300

    remove temporariamente o uso do cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 const loginRoute = require('../routes/loginRoute');
 const productsRoute = require('../routes/productsRoute');
@@ -9,7 +9,7 @@ const productsRoute = require('../routes/productsRoute');
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // app.use('/', (req, res) => res.send('Express API running on Vercel'));
 app.use('/login', loginRoute)
