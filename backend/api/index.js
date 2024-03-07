7133ab2178fc67f86df5593b1b9df472a7c3367c
Lucasteisouza <lucasteisouza@gmail.com>
2024-03-07 15:00:49 -0300

    modifica estrutura para adequar à necessidade do vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,23 @@
-const app = require('../app');
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const loginRoute = require('../routes/loginRoute');
+const productsRoute = require('../routes/productsRoute');
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', (req, res) => res.send('Express API running on Vercel'));
+app.use('/login', loginRoute)
+app.use('/products', productsRoute)
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+module.exports = app;
